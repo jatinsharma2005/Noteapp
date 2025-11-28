1e82dff4c33760f1535e6bf50dbabaa777b8074a
jatin <jatinkumar20056@gmail.com>
2025-11-28 22:12:50 +0530

    fix verify prerender issue

--- app/verify/page.jsx
+++ app/verify/page.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import VerifyForm from "./VerifyForm";
 
 export default function VerifyPage() {
